bfcf6adf7c0ad5fa461c08aba647754f5e4fb23f
Карасев Николай <karasev.n@zenden.ru>
2023-06-14 15:43:44 +0300

    solved Example02

--- Example02/Program.cs
+++ Example02/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.Write("Введите число: ");
+int anyNumber = Convert.ToInt32(Console.ReadLine());
+string anyNumberText = Convert.ToString(anyNumber);
+if (anyNumberText.Length > 2){
+  Console.WriteLine("третья цифра -> " + anyNumberText[2]);
+}
+else {
+  Console.WriteLine("-> третьей цифры нет");
+}
+
+
+
