de1fc27fe50a0f6405f616caf009d8cf022dfffa
Карасев Николай <karasev.n@zenden.ru>
2023-06-15 09:33:50 +0300

    Solved

--- Example01/Program.cs
+++ Example01/Program.cs
@@ -1,30 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-// Console.WriteLine("Hello, World!");
-// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
-
-
-Console.Write("Введите трехзначное число: ");
-int a = int.Parse(Console.ReadLine());
-
-
-    if (a < 100 || a >= 1000)
-    {
-    Console.Write("Вы ввели не трехзначное число, пожалуйста повторите ввод");
-    return;
-    }
-
-
-Console.WriteLine($"Введенное число: {a}");
-
-int b = (a / 10) % 10;
-
-Console.WriteLine("Вторая цифра: ");
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
 
 
 
-
-/*Console.WriteLine("Задача 10 ");
 Console.Write("Введи трёхзначное число: ");
-int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
-string stringNumber = Convert.ToString(threeDigitNumber);
-Console.WriteLine("вторая цифра этого числа -> "+stringNumber[1]);*/
+int a = Convert.ToInt32(Console.ReadLine());
+string b = Convert.ToString(a);
+Console.WriteLine("вторая цифра этого числа -> "+b[1]);
+
+
+
+
--- Example02/Program.cs
+++ Example02/Program.cs
@@ -1,5 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.Write("Введите число: ");
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает что третьей цифры нет.
+
+
+Console.WriteLine("Задача 13 ");
+
+Console.Write("Введи число: ");
 int anyNumber = Convert.ToInt32(Console.ReadLine());
 string anyNumberText = Convert.ToString(anyNumber);
 if (anyNumberText.Length > 2){
@@ -8,6 +12,5 @@ if (anyNumberText.Length > 2){
 else {
   Console.WriteLine("-> третьей цифры нет");
 }
-
 
 
